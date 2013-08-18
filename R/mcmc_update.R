ee6c4da1f89abd018a442b388b072ae73301b5b9
Hana Sevcikova <hanas@uw.edu>
2013-08-18 18:45:26 +0100

    zero-log handling

--- R/mcmc_update.R
+++ R/mcmc_update.R
@@ -115,8 +115,8 @@ mcmc.update.Triangle_c4 <- function(country, mcmc) {
 # within country loop, updates Triangle_c4
 # thus also the eps
 # (thus also lambda_c and the NAs in the eps!)
-        Triangle_c4_trans <- log(max(mcmc$Triangle_c4[country] - mcmc$meta$Triangle_c4.low, 1e-7)/
-                        max(mcmc$meta$Triangle_c4.up - mcmc$Triangle_c4[country], 1e-7))
+        Triangle_c4_trans <- log(max(mcmc$Triangle_c4[country] - mcmc$meta$Triangle_c4.low, 1e-20)/
+                        max(mcmc$meta$Triangle_c4.up - mcmc$Triangle_c4[country], 1e-20))
         epsT.idx <- mcmc$meta$start_c[country]:(mcmc$meta$lambda_c[country]-1)
         lepsT.idx <- length(epsT.idx)
 #        z <- (log_cond_Triangle_c4_trans(Triangle_c4_trans, 
