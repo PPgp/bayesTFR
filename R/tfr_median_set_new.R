607b48467c698c2cc09264cc51d9bb535f51d8cf
Hana Sevcikova <hanas@uw.edu>
2021-05-19 18:02:45 -0700

    fixed adjusting estimation function

--- R/tfr_median_set_new.R
+++ R/tfr_median_set_new.R
@@ -4,7 +4,7 @@ get.tfr.shift.estimation <- function(country.code, meta)
   return(meta$median.shift[[as.character(country.code)]])
 }
 
-tfr.median.set.all <- function(sim.dir, country, values, years=NULL)
+tfr.median.set.all <- function(sim.dir, country, values, years=NULL, burnin = 0, thin = 1)
 {
   mcmc.set <- get.tfr.mcmc(sim.dir)
   meta <- mcmc.set$meta
@@ -14,6 +14,9 @@ tfr.median.set.all <- function(sim.dir, country, values, years=NULL)
   bdem.shift.estimation <- do.call('get.tfr.shift.estimation', list(country.obj$code, meta))
   estimation.years <- as.numeric(dimnames(meta$tfr_matrix)[[1]])
   nr.estimation <- length(estimation.years)
+  bdem.shift <- NULL
+  nr.proj <- 0
+  has.uncertainty <- !is.null(mcmc.set$mcmc.list[[1]]$uncertainty) && mcmc.set$mcmc.list[[1]]$uncertainty
   if (has.predictions)
   {
     pred <- get.tfr.prediction(sim.dir = sim.dir)
@@ -21,12 +24,11 @@ tfr.median.set.all <- function(sim.dir, country, values, years=NULL)
     bdem.shift <- do.call('get.tfr.shift', list(country.obj$code, pred))
     nr.proj <- pred$nr.projections + 1
   }
-  else if(is.null(mcmc.set$mcmc.list[[1]]$uncertainty) || !mcmc.set$mcmc.list[[1]]$uncertainty)
-    stop('No estimation and no prediction to adjust.')
+  else if(!has.uncertainty) stop('No estimation and no prediction to adjust.')
   if(is.null(country.obj$name)) stop('Country not found.')
   if(is.null(years)) 
   {
-    if (!is.null(mcmc.set$mcmc.list[[1]]$uncertainty) && mcmc.set$mcmc.list[[1]]$uncertainty) years <- c(years, estimation.years)
+    if (has.uncertainty) years <- c(years, estimation.years)
     if (has.predictions) years <- c(years, pred.years[2:nr.proj])
   }
   mid.years <- years
@@ -37,15 +39,11 @@ tfr.median.set.all <- function(sim.dir, country, values, years=NULL)
   which.estimation <- is.element(years, estimation.years)
   if (any(which.estimation))
   {
-    if (is.null(mcmc.set$mcmc.list[[1]]$uncertainty) || !mcmc.set$mcmc.list[[1]]$uncertainty)
+    if (!has.uncertainty)
       stop("Provided years for estimation but uncertainty is not considered in the modeling step")
-    else
-    {
-      tfr.object <- get.tfr.estimation(mcmc.list = mcmc.set, country.code = country.obj$code, probs = 0.5)
-      estimation.medians <- as.numeric(unlist(tfr.object$tfr_quantile[,1]))
-      bdem.shift.estimation[is.element(estimation.years, years)] <- values[which.estimation] - 
-        estimation.medians[is.element(estimation.years, years)]
-    }
+    tfr.object <- get.tfr.estimation(mcmc.list = mcmc.set, country.code = country.obj$code, probs = 0.5, burnin = burnin, thin = thin)
+    estimation.medians <- as.numeric(unlist(tfr.object$tfr_quantile[,1]))
+    bdem.shift.estimation[is.element(estimation.years, years)] <- values[which.estimation] - estimation.medians[is.element(estimation.years, years)]
   }
   if (has.predictions) 
   {
@@ -59,7 +57,7 @@ tfr.median.set.all <- function(sim.dir, country, values, years=NULL)
   if (sum(bdem.shift) == 0) bdem.shift <- NULL
   if (sum(bdem.shift.estimation) == 0) bdem.shift.estimation <- NULL
   output <- list()
-  if (!is.null(mcmc.set$mcmc.list[[1]]$uncertainty) && mcmc.set$mcmc.list[[1]]$uncertainty)
+  if (has.uncertainty)
   {
     meta$median.shift.estimation[[as.character(country.obj$code)]] <- bdem.shift.estimation
     store.bayesTFR.meta.object(meta, meta$output.dir)
